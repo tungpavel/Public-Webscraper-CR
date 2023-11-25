606c8ed00cb7d5f2059989285e0b467e56c08255
tungpavel <107935279+tungpavel@users.noreply.github.com>
2023-11-25 22:07:41 +0000

    upDATE

--- SQL_query.sql
+++ SQL_query.sql
@@ -1,14 +1,15 @@
 SELECT
+  real_name,
   card_name,
   card_price,
   entry_date,
-  COALESCE(card_price - LEAD(card_price) OVER (PARTITION BY card_name ORDER BY entry_date DESC), 0) AS price_change,
-  COALESCE(round(CAST(card_price AS FLOAT) / LEAD(card_price) OVER (PARTITION BY card_name ORDER BY entry_date DESC), 2),1) AS change
+  COALESCE(card_price - LEAD(card_price) OVER (PARTITION BY real_name ORDER BY entry_date DESC), 0) AS price_change,
+  COALESCE(round(CAST(card_price AS FLOAT) / LEAD(card_price) OVER (PARTITION BY real_name ORDER BY entry_date DESC), 2),1) AS change
 FROM
   card_data
-WHERE card_name in (
-	SELECT card_name
+WHERE real_name in (
+	SELECT real_name
 	FROM card_data
 	WHERE entry_date >= "2023-11-22%")
 ORDER BY
-  card_name, entry_date DESC;
+  real_name, entry_date DESC;
